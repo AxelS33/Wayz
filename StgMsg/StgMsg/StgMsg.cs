1f364dc86ec4eaed29c99f2aa3201d40f757b20c
Adrien <adrien.dayre@viacesi.fr>
2014-05-13 17:09:04 +0200

    StgMsg

---
+++ StgMsg/StgMsg/StgMsg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StgMsg
+{
+    public class StgMsg
+    {
+        private string appName { get; set; } 
+        private string invoke { get; set; }
+        private string pSecurity { get; set; }
+        private bool statut { get; set; }
+        private string info { get; set; }
+        private object[] data { get; set; }
+        private string token { get; set; }
+        private string servName { get; set; }
+
+    }
+    public StgMsg() 
+    {
+        
+    }
+}
